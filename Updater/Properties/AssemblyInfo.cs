9a197a0987c64ec547e8b793e87796364372d870
M1XT3NZ <glpfan90@gmail.com>
2019-11-27 07:03:15 +0100

    update

---
+++ Updater/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+using System.Resources;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+
+[assembly: CompilationRelaxations(8)]
+[assembly: RuntimeCompatibility(WrapNonExceptionThrows = true)]
+[assembly: Debuggable(DebuggableAttribute.DebuggingModes.Default | DebuggableAttribute.DebuggingModes.DisableOptimizations | DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints | DebuggableAttribute.DebuggingModes.EnableEditAndContinue)]
+[assembly: AssemblyTitle("Updater Craften Minecraft")]
+[assembly: AssemblyDescription("Updater for the Craften Minecraft Application")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Updater Craften Minecraft")]
+[assembly: AssemblyCopyright("Copyright ©  2019-2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("4e209700-14e5-48d5-a86a-eaa777f17894")]
+[assembly: AssemblyFileVersion("0.0.0.125")]
+[assembly: AssemblyVersion("1.552.235.556")]
+[assembly: NeutralResourcesLanguage("de")]
